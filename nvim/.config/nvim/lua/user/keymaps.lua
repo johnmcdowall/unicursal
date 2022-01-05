67481d84a7c9ddbb96175731d04b368bb79008e8
John McDowall <j@jmd.fm>
2022-01-04 17:57:15 -0800

    Double tap leader to switch between current and previous file

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -10,6 +10,8 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+keymap("n", "<Leader><Leader>", "<C-^>", opts)
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
