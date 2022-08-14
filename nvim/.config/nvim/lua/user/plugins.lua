bba7a87f1f79b4c275a1e73652726900e5ea000c
John McDowall <j@jmd.fm>
2022-08-14 12:31:09 -0700

    Sigh

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -88,7 +88,6 @@ return packer.startup(function(use)
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
   }
   use "JoosepAlviste/nvim-ts-context-commentstring"
 
